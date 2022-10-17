8e98c1ed9c1e8679e621cd3a24cafaa865703305
sanches311 <itchipset@gmail.com>
2022-10-17 16:24:31 +0300

    feat: count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -15,11 +15,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function countCats(matrix) {
-  
-  let count=0;  
-  matrix.forEach((item) => {
-    if (item == "^^") {count++}
-  })
+  let count=0;
+  for (let i=0; i<matrix.length;i++) {
+      for (let j=0; j<matrix[0].length;j++) {
+          if (matrix[i][j] === "^^") {
+              count++;
+          }
+      }
+  }
+  //console.log(matrix[0].length);
   return count;
 }
 
